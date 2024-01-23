74550242df910a7d60bab8fb9be6f88a53e9c912
elmar <elmar@elmar-Air.bbrouter>
2024-01-23 11:09:04 +0400

    tapsiriq yerine yetirildi

---
+++ StaticTask/StaticTask/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace StaticTask;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string fullName;
+        do
+        {
+            Console.WriteLine("enter FullName:");
+
+             fullName = Console.ReadLine();
+
+        } while (!Student.CheckFullname(fullName));
+
+
+        string groupNo;
+        do
+        {
+            Console.WriteLine("enter GroupNo:");
+             groupNo = Console.ReadLine();
+
+        } while (!Student.CheckGroupNo(groupNo));
+
+        byte age;
+
+        string strAge;
+        do
+        {
+            Console.WriteLine("enter Age:");
+            strAge = Console.ReadLine();
+
+        } while (!byte.TryParse(strAge,out age) || age<0);
+
+        Student student = new Student(fullName,groupNo, age);
+
+        Console.WriteLine("Your FullName:"+student.FullName);
+        Console.WriteLine("Your GroupNo:"+student.GroupNo);
+        Console.WriteLine("Your Age:"+student.Age);
+
+
+        Console.ReadLine();
+    }
+
+}
+
---
+++ StaticTask/StaticTask/Student.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+
+namespace StaticTask
+{
+	public class Student
+	{
+
+		public string FullName;
+		public string GroupNo;
+		public byte Age;
+
+        public Student(string fullname,string groupno,byte age)
+        {
+            this.Age = age;
+            this.GroupNo = groupno;
+            this.FullName = fullname;
+
+        }
+
+        public static bool CheckGroupNo(string groupNo)
+        {
+            if (String.IsNullOrWhiteSpace(groupNo) || groupNo.Length != 4)
+                return false;
+
+            if (Char.IsLetter(groupNo[0]) && Char.IsUpper(groupNo[0]))
+            {
+                for (int i = 1; i < 4; i++)
+                {
+                    if (!Char.IsDigit(groupNo[i]))
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+            return false;
+
+        }
+        public static bool CheckFullname(string fullname)
+        {
+            if (String.IsNullOrWhiteSpace(fullname))
+                return false;
+
+            string[] nameParts = fullname.Split(' ');
+
+            if (nameParts.Length == 2)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    if (String.IsNullOrWhiteSpace(nameParts[i])|| CheckIsDigit(nameParts[i]))
+                    {
+                        return false;
+                    }              
+                }
+                return true;
+            }
+            return false;
+        }
+
+       public static bool CheckIsDigit(string name)
+        {
+            for (int i = 0; i < name.Length; i++)
+            {
+                if (char.IsDigit(name[i])) return true;
+                
+            }
+            return false;
+
+        }
+
+
+    }
+}
+
